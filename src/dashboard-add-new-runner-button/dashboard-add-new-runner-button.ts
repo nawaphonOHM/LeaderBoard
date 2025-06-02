1b4356d72bf9a3a9698d5b1a80b49f6ffdeec8e9
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-06-02 21:30:43 +0700

    request DashBoardAddNewRunnewCoordinatorRadioTower Service

--- src/dashboard-add-new-runner-button/dashboard-add-new-runner-button.ts
+++ src/dashboard-add-new-runner-button/dashboard-add-new-runner-button.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import {MatButton} from "@angular/material/button";
+import {
+  DashBoardAddNewRunnerCoordinatorRadioTower
+} from '../services/dash-board-add-new-runner-coordinator-radio-tower';
 
 @Component({
   selector: 'app-dashboard-add-new-runner-button',
@@ -10,5 +13,8 @@ import {MatButton} from "@angular/material/button";
   styleUrl: './dashboard-add-new-runner-button.scss'
 })
 export class DashboardAddNewRunnerButton {
+
+  constructor(readonly dashBoardAddNewRunnerButtonRadioTower: DashBoardAddNewRunnerCoordinatorRadioTower) {
+  }
 
 }
