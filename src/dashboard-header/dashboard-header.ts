f28edd2a5f3a3c8fde682a2a8001245a5c41bdce
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-06-02 16:18:17 +0700

    complete dashboard-header

--- src/dashboard-header/dashboard-header.ts
+++ src/dashboard-header/dashboard-header.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {MatCardHeader, MatCardTitle} from "@angular/material/card";
 
 @Component({
   selector: 'app-dashboard-header',
-  imports: [],
+    imports: [
+        MatCardHeader,
+        MatCardTitle
+    ],
   templateUrl: './dashboard-header.html',
   styleUrl: './dashboard-header.scss'
 })
