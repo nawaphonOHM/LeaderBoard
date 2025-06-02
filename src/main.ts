7a407bda08dd33cf634b4cbb7034b90dd7ae2b12
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-06-02 16:25:21 +0700

    update main setting

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import {Dashboard} from './dashboard/dashboard';
 
-bootstrapApplication(App, appConfig)
+bootstrapApplication(Dashboard)
   .catch((err) => console.error(err));
