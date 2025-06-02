bfba4ba0110d7530c82be3f61761a3eaac355a46
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-06-02 21:50:24 +0700

    update to use DashBoardAddNewRunnerInformationEnvelop instead of unknow

--- src/services/dash-board-add-new-runner-coordinator-radio-tower.ts
+++ src/services/dash-board-add-new-runner-coordinator-radio-tower.ts
@@ -1,18 +1,19 @@
 import {Injectable} from '@angular/core';
 import {Observable, Subject} from 'rxjs';
+import {DashBoardAddNewRunnerInformationEnvelop} from '../interfaces/dash-board-add-new-runner-information-envelop';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashBoardAddNewRunnerCoordinatorRadioTower {
 
-  private readonly radio = new Subject<unknown>()
+  private readonly radio = new Subject<DashBoardAddNewRunnerInformationEnvelop<unknown>>()
 
-  requestNewObservable(): Observable<unknown> {
+  requestNewObservable(): Observable<DashBoardAddNewRunnerInformationEnvelop<unknown>> {
     return this.radio.asObservable()
   }
 
-  emitMessage(message: unknown) {
+  emitMessage(message: DashBoardAddNewRunnerInformationEnvelop<unknown>) {
     this.radio.next(message)
   }
 }
