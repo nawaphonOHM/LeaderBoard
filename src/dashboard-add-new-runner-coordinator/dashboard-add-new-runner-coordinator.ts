4d2a2f4b3f06d298929496bfba49fd394adcecc9
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-06-02 21:18:57 +0700

    complete DashboardAddNewRunnerCoordinator

--- src/dashboard-add-new-runner-coordinator/dashboard-add-new-runner-coordinator.ts
+++ src/dashboard-add-new-runner-coordinator/dashboard-add-new-runner-coordinator.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {
+  DashBoardAddNewRunnerCoordinatorRadioTower
+} from '../services/dash-board-add-new-runner-coordinator-radio-tower';
 
 @Component({
   selector: 'app-dashboard-add-new-runner-coordinator',
@@ -6,6 +10,16 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard-add-new-runner-coordinator.html',
   styleUrl: './dashboard-add-new-runner-coordinator.scss'
 })
-export class DashboardAddNewRunnerCoordinator {
+export class DashboardAddNewRunnerCoordinator implements OnDestroy{
+
+  private readonly requestNewDialogListener: Subscription
+
+  constructor(readonly dashBoardAddNewRunnerCoordinatorRadioTower: DashBoardAddNewRunnerCoordinatorRadioTower) {
+    this.requestNewDialogListener = dashBoardAddNewRunnerCoordinatorRadioTower.requestNewObservable().subscribe()
+  }
+
+  ngOnDestroy(): void {
+        this.requestNewDialogListener.unsubscribe()
+    }
 
 }
