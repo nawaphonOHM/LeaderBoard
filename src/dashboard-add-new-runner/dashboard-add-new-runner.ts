84543e2e7616c57947a289dbf1745c9de6ba0037
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-06-02 20:03:28 +0700

    add dashboard-add-new-runner component

---
+++ src/dashboard-add-new-runner/dashboard-add-new-runner.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {MatButton} from "@angular/material/button";
+
+@Component({
+  selector: 'app-dashboard-add-new-runner',
+    imports: [
+        MatButton
+    ],
+  templateUrl: './dashboard-add-new-runner.html',
+  styleUrl: './dashboard-add-new-runner.scss'
+})
+export class DashboardAddNewRunner {
+
+}
