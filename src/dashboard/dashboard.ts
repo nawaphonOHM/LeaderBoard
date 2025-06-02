067d090f80bb2dd766e5eafdc8202d435f75c4cb
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-06-02 16:29:04 +0700

    add rendering dashboard-table component

--- src/dashboard/dashboard.ts
+++ src/dashboard/dashboard.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {MatCard} from '@angular/material/card';
 import {DashboardHeader} from '../dashboard-header/dashboard-header';
+import {DashboardTable} from '../dashboard-table/dashboard-table';
 
 @Component({
   selector: 'app-dashboard',
   imports: [
     MatCard,
-    DashboardHeader
+    DashboardHeader,
+    DashboardTable
   ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
