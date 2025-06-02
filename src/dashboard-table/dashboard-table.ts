1c2e91b514521aa9b7ae7c2448039e5d28832957
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-06-02 16:28:01 +0700

    add dashboard-table component

---
+++ src/dashboard-table/dashboard-table.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-table',
+  imports: [],
+  templateUrl: './dashboard-table.html',
+  styleUrl: './dashboard-table.scss'
+})
+export class DashboardTable {
+
+}
