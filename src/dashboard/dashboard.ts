aa102cc8d592e8ef681c2912289d04371a736dcc
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-06-02 20:04:09 +0700

    add render dashboard-card-add-runner

--- src/dashboard/dashboard.ts
+++ src/dashboard/dashboard.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import {MatCard, MatCardContent, MatCardHeader} from '@angular/material/card';
+import {MatCard, MatCardActions, MatCardContent, MatCardHeader} from '@angular/material/card';
 import {DashboardHeader} from '../dashboard-header/dashboard-header';
 import {DashboardTable} from '../dashboard-table/dashboard-table';
+import {DashboardAddNewRunner} from '../dashboard-add-new-runner/dashboard-add-new-runner';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,7 +11,9 @@ import {DashboardTable} from '../dashboard-table/dashboard-table';
     DashboardHeader,
     DashboardTable,
     MatCardHeader,
-    MatCardContent
+    MatCardContent,
+    MatCardActions,
+    DashboardAddNewRunner
   ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
