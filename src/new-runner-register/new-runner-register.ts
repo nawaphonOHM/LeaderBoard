94e973c78965b67fd03e39353b18f3e3c073e97c
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-06-02 22:40:10 +0700

    complete template NewRunnerRegister component

--- src/new-runner-register/new-runner-register.ts
+++ src/new-runner-register/new-runner-register.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, model} from '@angular/core';
+import {MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle} from '@angular/material/dialog';
+import {MatFormField, MatInput, MatLabel} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
+import {MatButton} from '@angular/material/button';
+import {MatDialogRef} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-new-runner-register',
-  imports: [],
+  imports: [
+    MatDialogTitle,
+    MatDialogContent,
+    MatFormField,
+    MatLabel,
+    MatInput,
+    FormsModule,
+    MatDialogActions,
+    MatButton,
+    MatDialogClose,
+  ],
   templateUrl: './new-runner-register.html',
   styleUrl: './new-runner-register.scss'
 })
 export class NewRunnerRegister {
+
+  protected readonly firstName = model('')
+
+  constructor(readonly matDialog: MatDialogRef<NewRunnerRegister>) {
+  }
+
+  cancelCallback() {
+    this.matDialog.close()
+  }
 
 }
