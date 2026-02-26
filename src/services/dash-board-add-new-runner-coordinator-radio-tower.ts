69d958d85bd2f05636631e2f2e4c9aea4a209f68
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2026-02-26 23:25:52 +0700

    refactor: replace Subject with Signal in runner coordinator service

--- src/services/dash-board-add-new-runner-coordinator-radio-tower.ts
+++ src/services/dash-board-add-new-runner-coordinator-radio-tower.ts
@@ -1,5 +1,4 @@
-import {Injectable} from '@angular/core';
-import {Observable, Subject} from 'rxjs';
+import {Injectable, Signal, signal} from '@angular/core';
 import {DashBoardAddNewRunnerInformationEnvelop} from '../interfaces/dash-board-add-new-runner-information-envelop';
 
 @Injectable({
@@ -7,13 +6,13 @@ import {DashBoardAddNewRunnerInformationEnvelop} from '../interfaces/dash-board-
 })
 export class DashBoardAddNewRunnerCoordinatorRadioTower {
 
-  private readonly radio = new Subject<DashBoardAddNewRunnerInformationEnvelop<unknown>>()
+  private readonly radio = signal<DashBoardAddNewRunnerInformationEnvelop<unknown> | null>(null)
 
-  requestNewObservable(): Observable<DashBoardAddNewRunnerInformationEnvelop<unknown>> {
-    return this.radio.asObservable()
+  requestNewObservable(): Signal<DashBoardAddNewRunnerInformationEnvelop<unknown> | null> {
+    return this.radio.asReadonly()
   }
 
   emitMessage(message: DashBoardAddNewRunnerInformationEnvelop<unknown>) {
-    this.radio.next(message)
+    this.radio.set(message);
   }
 }
