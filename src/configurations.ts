1d3f3db0adc3bf1a52b72542a956e9ccf8c382ad
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2026-03-14 22:41:23 +0700

    feat: add CONFIGURATION token with default factory to configurations module

--- src/configurations.ts
+++ src/configurations.ts
@@ -1,5 +1,13 @@
+import {InjectionToken} from '@angular/core';
 
 
 export interface Configuration {
   flagUrl: string
 }
+
+export const CONFIGURATION = new InjectionToken<Configuration>('CONFIGURATION', {
+  providedIn: 'root',
+  factory: () => ({
+    flagUrl: 'https://flagsapi.com/__nationality__/flat/32.png'
+  })
+})
