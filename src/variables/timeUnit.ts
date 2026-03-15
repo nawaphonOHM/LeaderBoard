ccf855be671ea7539aa7e9f25dc5148895273bab
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2026-03-15 13:05:38 +0700

    feat: add `TIME_UNIT` injection token with factory for time unit constants

--- src/variables/timeUnit.ts
+++ src/variables/timeUnit.ts
@@ -7,3 +7,12 @@ export interface TheTimeUnit {
   MILLISECONDS_IN_SECOND: number;
   SECOND_IN_MINUTE: number;
 }
+
+
+export const TIME_UNIT = new InjectionToken<TheTimeUnit>('TIME_UNIT', {
+  providedIn: 'root',
+  factory: () => ({
+    MILLISECONDS_IN_SECOND: 1000,
+    SECOND_IN_MINUTE: 60
+  })
+})
