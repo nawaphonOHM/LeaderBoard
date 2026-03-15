c6ba28a36677b5dffbb1c460f298a4e9e200c27a
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2026-03-15 13:05:49 +0700

    refactor: remove unused time unit constants from `timeUnit.ts`

--- src/variables/timeUnit.ts
+++ src/variables/timeUnit.ts
@@ -1,7 +1,5 @@
+import {InjectionToken} from '@angular/core';
 
-
-export const MILLISECONDS_IN_SECOND = 1000;
-export const SECOND_IN_MINUTE = 60;
 
 export interface TheTimeUnit {
   MILLISECONDS_IN_SECOND: number;
