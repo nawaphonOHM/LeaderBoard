a7dd854f344b01e52d5a8cc77de67d25986623ff
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2026-03-15 13:28:28 +0700

    refactor: remove unused import from `dash-board-add-new-runner-information-envelop.ts`

--- src/interfaces/dash-board-add-new-runner-information-envelop.ts
+++ src/interfaces/dash-board-add-new-runner-information-envelop.ts
@@ -1,6 +1,4 @@
-import {DashBoardAddNewRunnerData} from './dash-board-add-new-runner-data';
-
-export interface DashBoardAddNewRunnerInformationEnvelop<T>{
+export interface DashBoardAddNewRunnerInformationEnvelop<T> {
   state: 'SEND_REQUEST' | 'RESPONSE_DATA'
 
   data: T | null
