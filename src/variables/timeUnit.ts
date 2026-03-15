88a70a46ae6d3efd11f7d7e7e164ae6195be1773
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2026-03-15 13:04:58 +0700

    feat: add `TheTimeUnit` interface to define time unit constants structure

--- src/variables/timeUnit.ts
+++ src/variables/timeUnit.ts
@@ -2,3 +2,8 @@
 
 export const MILLISECONDS_IN_SECOND = 1000;
 export const SECOND_IN_MINUTE = 60;
+
+export interface TheTimeUnit {
+  MILLISECONDS_IN_SECOND: number;
+  SECOND_IN_MINUTE: number;
+}
