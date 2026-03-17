85a43ab879b0705394d0f884e1591102ed7c7199
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2026-03-17 21:55:08 +0700

    feat: add FORM_STATE enum to AddNewRunnerModalRadioTower service

--- src/services/add-new-runner-modal-radio-tower.ts
+++ src/services/add-new-runner-modal-radio-tower.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
 
+
+export enum FORM_STATE {
+  INPUTTING,
+  SAVING,
+  DONE,
+  NOP
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class AddNewRunnerModalRadioTower {
-  
+
 }
