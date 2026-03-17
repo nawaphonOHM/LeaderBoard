5c528a8166e157f8c9124c2bbf2a990c2c808fca
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2026-03-17 22:33:27 +0700

    feat: add effect to handle FORM_STATE.DONE in NewRunnerRegister component

--- src/dashboard/dashboard-add-new-runner-coordinator/new-runner-register/new-runner-register.ts
+++ src/dashboard/dashboard-add-new-runner-coordinator/new-runner-register/new-runner-register.ts
@@ -1,14 +1,19 @@
-import {Component, inject} from '@angular/core';
-import {MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle} from '@angular/material/dialog';
+import {Component, effect, inject} from '@angular/core';
+import {
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle
+} from '@angular/material/dialog';
 import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {MatButton} from '@angular/material/button';
-import {MatDialogRef} from '@angular/material/dialog';
 import {GeneralInput} from './general-input/general-input';
 import {Country, CountrySelectComponent} from '@wlucha/ng-country-select';
 import {TimeUsedForFinnishRunning} from './time-used-for-finnish-running/time-used-for-finnish-running';
 import {CONFIGURATION} from '../../../variables/configurations';
 import {UnexpectedToReachHere} from '../../../errors/UnexpectedToReachHere';
-import {AddNewRunnerModalRadioTower} from '../../../services/add-new-runner-modal-radio-tower';
+import {AddNewRunnerModalRadioTower, FORM_STATE} from '../../../services/add-new-runner-modal-radio-tower';
 
 @Component({
   selector: 'app-new-runner-register',
@@ -56,6 +61,30 @@ export class NewRunnerRegister {
   private readonly radioTower = inject(AddNewRunnerModalRadioTower)
 
   constructor() {
+
+    effect(() => {
+
+      const signal = this.radioTower.requestNewObservable()
+
+      if (signal() !== FORM_STATE.DONE) {
+        return
+      }
+
+      const rawInput = this.inputGroup.getRawValue()
+
+      if (rawInput.nationality?.alpha2 === undefined) {
+        console.log('invalid nationality')
+        throw new UnexpectedToReachHere("nationality should has a value.")
+      }
+
+      this.matDialog.close({
+        firstName: rawInput.firstName,
+        lastName: rawInput.lastName,
+        nationalityUrlImage: this.configuration.flagUrl.replaceAll("__nationality__", rawInput.nationality?.alpha2.toUpperCase()),
+        timeUsedInMillisecond: rawInput.timeUsedInMillisecond
+
+      })
+    })
   }
 
   cancelCallback() {
